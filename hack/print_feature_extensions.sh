4fca39647249c66ab433de07076e5ef02b8a187b
Vincent Riesop <v.riesop@sap.com>
2024-08-15 14:35:45 +0200

    add script to get non default filetypes for features

---
+++ hack/print_feature_extensions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+search_and_print_directories() {
+    local pattern="$1"
+    local base_pattern="${pattern%%.*}" 
+    
+    while IFS= read -r file; do
+        dir=$(dirname "$file" | sed 's|^\./||')
+        
+        suffix="${file##*/}"
+        suffix="${suffix#"$base_pattern"}" 
+        
+        echo "('$dir', '$suffix'),"
+    done < <(find . -type f -name "$pattern" | sort -u)
+}
+
+
+echo "All features with a non-default image archive filetype:"
+search_and_print_directories "image.*"
+
+echo "All features with a non-default image filetype:"
+search_and_print_directories "convert.*"
