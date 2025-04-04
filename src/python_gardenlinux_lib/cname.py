2e702329601ea66ff617ae76bfe08483fa594526
Tobias Wolf <wolf@b1-systems.de>
2025-04-04 08:23:54 +0200

    Add support to parse and return the GardenLinux "cname"
    Relates: https://github.com/gardenlinux/gardenlinux/issues/2830
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

---
+++ src/python_gardenlinux_lib/cname.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+from .features import parse_features
+
+from functools import reduce
+from os.path import basename, dirname
+
+import argparse
+import re
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--arch", dest="arch")
+    parser.add_argument("--feature-dir", default="features")
+    parser.add_argument("--version", dest="version")
+    parser.add_argument("cname")
+
+    args = parser.parse_args()
+
+    re_match = re.match(
+        "([a-zA-Z0-9]+(-[a-zA-Z0-9\\_\\-]*?)?)(-([a-z0-9]+)(-([a-z0-9.]+)-([a-z0-9]+))*)?$",
+        args.cname
+    )
+
+    assert re_match, f"not a valid cname {args.cname}"
+
+    if re_match.lastindex == 1:
+        cname_base, arch = re_match[1].split("-", 1)
+        commit_id = None
+        version = None
+    else:
+        arch = re_match[4]
+        cname_base = re_match[1]
+        commit_id = re_match[7]
+        version = re_match[6]
+
+    if args.arch is not None:
+        arch = args.arch
+
+    if args.version is not None:
+        re_match = re.match("([a-z0-9.]+)(-([a-z0-9]+))?$", args.cname)
+        assert re_match, f"not a valid version {args.version}"
+
+        commit_id = re_match[3]
+        version = re_match[1]
+
+    gardenlinux_root = dirname(args.feature_dir)
+    feature_dir_name = basename(args.feature_dir)
+
+    if gardenlinux_root == "":
+        gardenlinux_root = "."
+
+    graph = parse_features.get_features_graph(
+        cname_base, gardenlinux_root, feature_dir_name
+    )
+
+    sorted_features = parse_features.sort_nodes(graph)
+
+    minimal_feature_set = get_minimal_feature_set(graph)
+
+    sorted_minimal_features = parse_features.sort_set(
+        minimal_feature_set, sorted_features
+    )
+
+    cname_base = get_cname_base(sorted_minimal_features)
+
+    cname = f"{cname_base}-{arch}"
+    if commit_id is not None:
+        cname += f"-{version}-{commit_id}"
+
+    print(cname)
+
+def get_cname_base(sorted_features):
+    return reduce(
+        lambda a, b : a + ("-" if not b.startswith("_") else "") + b, sorted_features
+    )
+
+def get_minimal_feature_set(graph):
+    return set([node for (node, degree) in graph.in_degree() if degree == 0])
+
+
+if __name__ == "__main__":
+    main()
