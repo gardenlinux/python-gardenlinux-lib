4cd8911387cebfe8490e58d198ed8de85bc115c5
Tobias Wolf <wolf@b1-systems.de>
2025-05-15 17:48:28 +0200

    Parse `cname` without any separators correctly
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- src/gardenlinux/features/cname_main.py
+++ src/gardenlinux/features/cname_main.py
@@ -26,7 +26,7 @@ def main():
     args = parser.parse_args()
 
     re_match = re.match(
-        "([a-zA-Z0-9]+([\\_\\-][a-zA-Z0-9]+)+?)(-([a-z0-9]+)(-([a-z0-9.]+)-([a-z0-9]+))*)?$",
+        "([a-zA-Z0-9]+([\\_\\-][a-zA-Z0-9]+)*?)(-([a-z0-9]+)(-([a-z0-9.]+)-([a-z0-9]+))*)?$",
         args.cname,
     )
 
