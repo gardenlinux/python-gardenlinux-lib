2d80a7412eb680c0abb779f896c742bbff950e52
Tobias Wolf <wolf@b1-systems.de>
2025-05-26 09:48:48 +0200

    Move "features" related test to a sub-package
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- tests/features/test_features_parser.py
+++ tests/features/test_features_parser.py
@@ -1,7 +1,7 @@
 import pytest
 
 from gardenlinux.features import Parser
-from .constants import GL_ROOT_DIR
+from ..constants import GL_ROOT_DIR
 
 
 @pytest.mark.parametrize(
@@ -58,13 +58,13 @@ from .constants import GL_ROOT_DIR
         ),
     ],
 )
-def test_get_features_dict(input_cname: str, expected_output: dict):
+def test_parser_filter_as_dict(input_cname: str, expected_output: dict):
     """
-    Tests if get_features_dict returns the dict with expected features.
+    Tests if parser_filter_as_dict returns the dict with expected features.
 
     If you discover that this test failed, you may want to verify if the included
     features have changed since writing this test. In this case, update the expected output accordingly.
-    You can print the output of get_features_dict so you have the dict in the expected format.
+    You can print the output of parser_filter_as_dict so you have the dict in the expected format.
     """
     features_dict = Parser(GL_ROOT_DIR).filter_as_dict(input_cname)
     assert features_dict == expected_output
