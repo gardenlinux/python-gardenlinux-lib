2b7659ca2c9328f8ac65fde3670c5cb437d90ed0
Tobias Wolf <wolf@b1-systems.de>
2025-10-02 09:27:11 +0200

    Add support to interact with a local podman installation
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- src/gardenlinux/oci/__main__.py
+++ src/gardenlinux/oci/__main__.py
@@ -4,12 +4,15 @@
 gl-oci main entrypoint
 """
 
+import json
 from typing import List
 
 import click
 
 from .container import Container
 from .image_manifest import ImageManifest
+from .podman import Podman
+from .podman_context import PodmanContext
 
 
 @click.group()
@@ -21,6 +24,306 @@ def cli() -> None:
     """
 
     pass
+
+
+@cli.command()
+@click.option(
+    "--index",
+    required=True,
+    help="OCI image index",
+)
+@click.option(
+    "--index-tag",
+    required=True,
+    help="OCI image index tag",
+)
+@click.option(
+    "--container",
+    required=True,
+    help="Container Name",
+)
+@click.option(
+    "--tag",
+    required=True,
+    help="OCI tag of image",
+)
+@click.option(
+    "--insecure",
+    type=bool,
+    default=False,
+    help="Use HTTP to communicate with the registry",
+)
+@click.option(
+    "--additional_tag",
+    required=False,
+    multiple=True,
+    help="Additional tag to push the index with",
+)
+def add_container_to_index(
+    index: str,
+    index_tag: str,
+    container: str,
+    tag: str,
+    insecure: bool,
+    additional_tag: List[str],
+) -> None:
+    """
+    Adds an image container to an OCI image index.
+
+    :since: 1.0.0
+    """
+
+    manifest_container = Container(f"{container}:{tag}", insecure=insecure)
+
+    manifest = manifest_container.read_manifest()
+
+    index_resource = Container(index, insecure=insecure)
+
+    image_index = index_resource.read_or_generate_index()
+    image_index.append_manifest(manifest)
+
+    index_resource.push_index(image_index)
+    index_resource.push_index_for_tags(image_index, additional_tag)
+
+
+@cli.command()
+@click.option(
+    "--container",
+    required=True,
+    help="Container Name",
+)
+@click.option(
+    "--tag",
+    required=True,
+    help="OCI tag of image",
+)
+@click.option(
+    "--dir",
+    "directory",
+    required=True,
+    type=click.Path(),
+    help="Path to the build Containerfile",
+)
+@click.option(
+    "--platform",
+    required=False,
+    help="OCI platform as os/arch/variant",
+)
+@click.option(
+    "--additional_tag",
+    required=False,
+    multiple=True,
+    help="Additional tag to push the manifest with",
+)
+@click.option(
+    "--build_arg",
+    required=False,
+    default=[],
+    multiple=True,
+    help="Additional build args for Containerfile",
+)
+@click.option(
+    "--oci_archive",
+    required=False,
+    help="Write build result to the OCI archive path and file name",
+)
+def build_container(
+    container: str,
+    tag: str,
+    directory: str,
+    platform: str,
+    additional_tag: List[str],
+    build_arg: List[str],
+    oci_archive: str,
+) -> None:
+    """
+    Build an OCI container based on the defined `Containerfile`.
+
+    :since: 1.0.0
+    """
+
+    podman = Podman()
+
+    with PodmanContext() as podman_context:
+        if oci_archive is None:
+            image_id = podman.build(
+                directory,
+                podman=podman_context,
+                platform=platform,
+                oci_tag=f"{container}:{tag}",
+                build_args=Podman.parse_build_args_list(build_arg),
+            )
+        else:
+            build_result_data = podman.build_and_save_oci_archive(
+                directory,
+                oci_archive,
+                podman=podman_context,
+                platform=platform,
+                oci_tag=f"{container}:{tag}",
+                build_args=Podman.parse_build_args_list(build_arg),
+            )
+
+            _, image_id = build_result_data.popitem()
+
+        if additional_tag is not None:
+            podman.tag_list(
+                image_id, Podman.get_container_tag_list(container, additional_tag)
+            )
+
+    print(image_id)
+
+
+@cli.command()
+@click.option(
+    "--oci_archive",
+    required=False,
+    help="Write build result to the OCI archive path and file name",
+)
+@click.option(
+    "--additional_tag",
+    required=False,
+    multiple=True,
+    help="Additional tag to push the manifest with",
+)
+def load_container(oci_archive: str, additional_tag: List[str]) -> None:
+    """
+    Push to an OCI registry.
+
+    :since: 1.0.0
+    """
+
+    podman = Podman()
+
+    with PodmanContext() as podman_context:
+        image_id = podman.load_oci_archive(oci_archive, podman=podman_context)
+
+        if additional_tag is not None:
+            podman.tag_list(image_id, additional_tag, podman=podman_context)
+
+    print(image_id)
+
+
+@cli.command()
+@click.option(
+    "--dir",
+    "directory",
+    required=True,
+    type=click.Path(),
+    help="path to the build artifacts",
+)
+def load_containers_from_directory(directory: str) -> None:
+    """
+    Push to an OCI registry.
+
+    :since: 1.0.0
+    """
+
+    result = Podman().load_oci_archives_from_directory(directory)
+    print(json.dumps(result))
+
+
+@cli.command()
+@click.option(
+    "--index",
+    required=True,
+    help="OCI image index",
+)
+@click.option(
+    "--index-tag",
+    required=True,
+    help="OCI image index tag",
+)
+@click.option(
+    "--insecure",
+    type=bool,
+    default=False,
+    help="Use HTTP to communicate with the registry",
+)
+@click.option(
+    "--additional_tag",
+    required=False,
+    multiple=True,
+    help="Additional tag to push the index with",
+)
+def new_index(
+    index: str, index_tag: str, insecure: bool, additional_tag: List[str]
+) -> None:
+    """
+    Push a list of files from the `manifest_folder` to an index.
+
+    :since: 1.0.0
+    """
+
+    index_resource = Container(f"{index}:{index_tag}", insecure=insecure)
+
+    image_index = index_resource.generate_index()
+    index_resource.push_index(image_index)
+    index_resource.push_index_for_tags(image_index, additional_tag)
+
+
+@cli.command()
+@click.option(
+    "--container",
+    required=True,
+    help="Container Name",
+)
+@click.option(
+    "--tag",
+    required=False,
+    help="OCI tag of image",
+)
+@click.option(
+    "--platform",
+    required=False,
+    help="OCI platform as os/arch/variant",
+)
+@click.option(
+    "--insecure",
+    type=bool,
+    default=False,
+    help="Use HTTP to communicate with the registry",
+)
+def pull_container(container: str, tag: str, platform: str, insecure: bool) -> None:
+    """
+    Push to an OCI registry.
+
+    :since: 1.0.0
+    """
+
+    image_id = Podman(insecure=insecure).pull(container, oci_tag=tag, platform=platform)
+    print(image_id)
+
+
+@cli.command()
+@click.option(
+    "--container",
+    required=True,
+    help="Container Name",
+)
+@click.option(
+    "--tag",
+    required=False,
+    help="OCI tag of image",
+)
+@click.option(
+    "--destination",
+    required=False,
+    help="OCI container destination",
+)
+@click.option(
+    "--insecure",
+    type=bool,
+    default=False,
+    help="Use HTTP to communicate with the registry",
+)
+def push_container(container: str, tag: str, destination: str, insecure: bool) -> None:
+    """
+    Push to an OCI registry.
+
+    :since: 1.0.0
+    """
+
+    Podman(insecure=insecure).push(container, oci_tag=tag, destination=destination)
 
 
 @cli.command()
@@ -58,13 +361,13 @@ def push_index_from_directory(
     additional_tag: List[str],
 ) -> None:
     """
-    Push a list of files from the `manifest_folder` to an index.
+    Pushes manifests stored in a directory to a given OCI image index.
 
     :since: 0.10.9
     """
 
-    index = Container(f"{index}:{index_tag}", insecure=insecure)
-    index.push_index_from_directory(manifest_folder, additional_tag)
+    index_resource = Container(f"{index}:{index_tag}", insecure=insecure)
+    index_resource.push_index_from_directory(manifest_folder, additional_tag)
 
 
 @cli.command()
@@ -96,44 +399,44 @@ def push_index_tags(index: str, index_tag: str, insecure: bool, tag: List[str]) 
     :since: 0.10.9
     """
 
-    index = Container(f"{index}:{index_tag}", insecure=insecure)
+    index_resource = Container(f"{index}:{index_tag}", insecure=insecure)
 
-    image_index = index.read_or_generate_index()
-    index.push_index_for_tags(image_index, tag)
+    image_index = index_resource.read_or_generate_index()
+    index_resource.push_index_for_tags(image_index, tag)
 
 
 @cli.command()
 @click.option(
     "--container",
     required=True,
-    type=click.Path(),
     help="Container Name",
 )
-@click.option(
-    "--cname", required=True, type=click.Path(), help="Canonical Name of Image"
-)
+@click.option("--cname", required=True, help="Canonical Name of Image")
 @click.option(
     "--arch",
     required=False,
-    type=click.Path(),
     default=None,
     help="Target Image CPU Architecture",
 )
 @click.option(
     "--version",
     required=False,
-    type=click.Path(),
     default=None,
     help="Version of image",
 )
 @click.option(
     "--commit",
     required=False,
-    type=click.Path(),
     default=None,
     help="Commit of image",
 )
-@click.option("--dir", "directory", required=True, help="path to the build artifacts")
+@click.option(
+    "--dir",
+    "directory",
+    required=True,
+    type=click.Path(),
+    help="path to the build artifacts",
+)
 @click.option(
     "--cosign_file",
     required=False,
@@ -141,11 +444,13 @@ def push_index_tags(index: str, index_tag: str, insecure: bool, tag: List[str]) 
 )
 @click.option(
     "--manifest_file",
+    type=click.Path(),
     default="manifests/manifest.json",
     help="A file where the index entry for the pushed manifest is written to.",
 )
 @click.option(
     "--insecure",
+    type=bool,
     default=False,
     help="Use HTTP to communicate with the registry",
 )
@@ -192,39 +497,35 @@ def push_manifest(
 @click.option(
     "--container",
     required=True,
-    type=click.Path(),
     help="Container Name",
 )
 @click.option(
     "--cname",
     required=False,
-    type=click.Path(),
     default=None,
     help="Canonical Name of Image",
 )
 @click.option(
     "--arch",
     required=False,
-    type=click.Path(),
     default=None,
     help="Target Image CPU Architecture",
 )
 @click.option(
     "--version",
     required=False,
-    type=click.Path(),
     default=None,
     help="Version of image",
 )
 @click.option(
     "--commit",
     required=False,
-    type=click.Path(),
     default=None,
     help="Commit of image",
 )
 @click.option(
     "--insecure",
+    type=bool,
     default=False,
     help="Use HTTP to communicate with the registry",
 )
@@ -253,6 +554,72 @@ def push_manifest_tags(
 
     manifest = container.read_or_generate_manifest(cname, arch, version, commit)
     container.push_manifest_for_tags(manifest, tag)
+
+
+@cli.command()
+@click.option(
+    "--container",
+    required=True,
+    help="Container Name",
+)
+@click.option(
+    "--tag",
+    required=False,
+    help="OCI tag of image",
+)
+@click.option(
+    "--oci_archive",
+    required=False,
+    help="Write build result to the OCI archive path and file name",
+)
+def save_container(container: str, tag: str, oci_archive: str) -> None:
+    """
+    Push to an OCI registry.
+
+    :since: 1.0.0
+    """
+
+    podman = Podman()
+
+    image_id = podman.get_image_id(container, oci_tag=tag)
+    podman.save_oci_archive(image_id, oci_archive, oci_tag=tag)
+
+
+@cli.command()
+@click.option(
+    "--container",
+    required=True,
+    help="Container Name",
+)
+@click.option(
+    "--tag",
+    required=False,
+    help="OCI tag of image",
+)
+@click.option(
+    "--additional_tag",
+    required=False,
+    multiple=True,
+    help="Additional tag to push the manifest with",
+)
+def tag_container(container: str, tag: str, additional_tag: List[str]) -> None:
+    """
+    Push to an OCI registry.
+
+    :since: 1.0.0
+    """
+
+    podman = Podman()
+
+    with PodmanContext() as podman_context:
+        image_id = podman.get_image_id(container, podman=podman_context, oci_tag=tag)
+
+        if additional_tag is not None:
+            podman.tag_list(
+                image_id,
+                Podman.get_container_tag_list(container, additional_tag),
+                podman=podman_context,
+            )
 
 
 def main() -> None:
