3fa702b6a3ff848b1d09ec86838d8be9dd611ab3
Vivus Ignis <ignis@posteo.de>
2025-10-16 10:34:14 +0200

    black-formatting applied + fixes as per PR review

--- tests/github/test_create_github_release_notes.py
+++ tests/github/test_create_github_release_notes.py
@@ -19,11 +19,13 @@ from ..constants import (
     TEST_GARDENLINUX_RELEASE,
 )
 
-TEST_FLAVORS = [("foo_bar_baz", "legacy"),
-                ("aws-gardener_prod_trustedboot_tpm2-amd64", "legacy"),
-                ("openstack-gardener_prod_tpm2_trustedboot-arm64", "tpm2_trustedboot"),
-                ("azure-gardener_prod_usi-amd64", "usi"),
-                ("", "legacy")]
+TEST_FLAVORS = [
+    ("foo_bar_baz", "legacy"),
+    ("aws-gardener_prod_trustedboot_tpm2-amd64", "legacy"),
+    ("openstack-gardener_prod_tpm2_trustedboot-arm64", "tpm2_trustedboot"),
+    ("azure-gardener_prod_usi-amd64", "usi"),
+    ("", "legacy"),
+]
 
 
 def test_release_notes_changes_section_empty_packagelist():
@@ -31,10 +33,11 @@ def test_release_notes_changes_section_empty_packagelist():
         m.get(
             f"{GLVD_BASE_URL}/patchReleaseNotes/{TEST_GARDENLINUX_RELEASE}",
             text='{"packageList": []}',
-            status_code=200
+            status_code=200,
         )
-        assert release_notes_changes_section(TEST_GARDENLINUX_RELEASE) == "", \
-            "Expected an empty result if GLVD returns an empty package list"
+        assert (
+            release_notes_changes_section(TEST_GARDENLINUX_RELEASE) == ""
+        ), "Expected an empty result if GLVD returns an empty package list"
 
 
 def test_release_notes_changes_section_broken_glvd_response():
@@ -42,54 +45,71 @@ def test_release_notes_changes_section_broken_glvd_response():
         m.get(
             f"{GLVD_BASE_URL}/patchReleaseNotes/{TEST_GARDENLINUX_RELEASE}",
             text="<html><body><h1>Personal Home Page</h1></body></html>",
-            status_code=200
+            status_code=200,
         )
-        assert "fill this in" in release_notes_changes_section(TEST_GARDENLINUX_RELEASE), \
-            "Expected a placeholder message to be generated if GVLD response is not valid"
+        assert "fill this in" in release_notes_changes_section(
+            TEST_GARDENLINUX_RELEASE
+        ), "Expected a placeholder message to be generated if GVLD response is not valid"
 
 
 def test_release_notes_compare_package_versions_section_legacy_versioning_is_recognized():
-    assert "Full List of Packages in Garden Linux version 1.0" in \
-        release_notes_compare_package_versions_section("1.0", {}), "Legacy versioning is supported"
+    assert (
+        "Full List of Packages in Garden Linux version 1.0"
+        in release_notes_compare_package_versions_section("1.0", {})
+    ), "Legacy versioning is supported"
 
 
-def test_release_notes_compare_package_versions_section_legacy_versioning_patch_release_is_recognized(monkeypatch):
+def test_release_notes_compare_package_versions_section_legacy_versioning_patch_release_is_recognized(
+    monkeypatch,
+):
     def mock_compare_apt_repo_versions(previous_version, current_version):
         output = f"| Package | {previous_version} | {current_version} |\n"
         output += "|---------|--------------------|-------------------|\n"
         output += "|containerd|1.0|1.1|\n"
         return output
 
-    monkeypatch.setattr("gardenlinux.github.release_notes.sections.compare_apt_repo_versions",
-                        mock_compare_apt_repo_versions)
+    monkeypatch.setattr(
+        "gardenlinux.github.release_notes.sections.compare_apt_repo_versions",
+        mock_compare_apt_repo_versions,
+    )
 
-    assert "|containerd|1.0|1.1|" in \
-        release_notes_compare_package_versions_section("1.1", {}), "Legacy versioning patch releases are supported"
+    assert "|containerd|1.0|1.1|" in release_notes_compare_package_versions_section(
+        "1.1", {}
+    ), "Legacy versioning patch releases are supported"
 
 
 def test_release_notes_compare_package_versions_section_semver_is_recognized():
-    assert "Full List of Packages in Garden Linux version 1.20.0" in \
-        release_notes_compare_package_versions_section("1.20.0", {}), "Semver is supported"
+    assert (
+        "Full List of Packages in Garden Linux version 1.20.0"
+        in release_notes_compare_package_versions_section("1.20.0", {})
+    ), "Semver is supported"
 
 
-def test_release_notes_compare_package_versions_section_semver_patch_release_is_recognized(monkeypatch):
+def test_release_notes_compare_package_versions_section_semver_patch_release_is_recognized(
+    monkeypatch,
+):
     def mock_compare_apt_repo_versions(previous_version, current_version):
         output = f"| Package | {previous_version} | {current_version} |\n"
         output += "|---------|--------------------|-------------------|\n"
         output += "|containerd|1.0|1.1|\n"
         return output
 
-    monkeypatch.setattr("gardenlinux.github.release_notes.sections.compare_apt_repo_versions",
-                        mock_compare_apt_repo_versions)
+    monkeypatch.setattr(
+        "gardenlinux.github.release_notes.sections.compare_apt_repo_versions",
+        mock_compare_apt_repo_versions,
+    )
 
-    assert "|containerd|1.0|1.1|" in \
-        release_notes_compare_package_versions_section("1.20.1", {}), "Semver patch releases are supported"
+    assert "|containerd|1.0|1.1|" in release_notes_compare_package_versions_section(
+        "1.20.1", {}
+    ), "Semver patch releases are supported"
 
 
 def test_release_notes_compare_package_versions_section_unrecognizable_version(caplog):
     assert release_notes_compare_package_versions_section("garden.linux", {}) is None
-    assert any("Unexpected version number format garden.linux" in
-               record.message for record in caplog.records), "Expected an error log message"
+    assert any(
+        "Unexpected version number format garden.linux" in record.message
+        for record in caplog.records
+    ), "Expected an error log message"
 
 
 @pytest.mark.parametrize("flavor", TEST_FLAVORS)
@@ -106,11 +126,16 @@ def test_github_release_page(monkeypatch, downloads_dir, release_s3_bucket):
 
     class SubmoduleAsRepo(Repo):
         """This will fake a git submodule as a git repository object."""
+
         def __new__(cls, *args, **kwargs):
             r = super().__new__(Repo)
             r.__init__(*args, **kwargs)
 
-            maybe_gl_submodule = [submodule for submodule in r.submodules if submodule.name.endswith("/gardenlinux")]
+            maybe_gl_submodule = [
+                submodule
+                for submodule in r.submodules
+                if submodule.name.endswith("/gardenlinux")
+            ]
             if not maybe_gl_submodule:
                 return r
             else:
@@ -120,28 +145,37 @@ def test_github_release_page(monkeypatch, downloads_dir, release_s3_bucket):
             sr.remotes.origin.pull("main")
             return sr
 
-    monkeypatch.setattr("gardenlinux.github.release_notes.helpers.Repo", SubmoduleAsRepo)
+    monkeypatch.setattr(
+        "gardenlinux.github.release_notes.helpers.Repo", SubmoduleAsRepo
+    )
     import gardenlinux.github
 
-    release_fixture_path = RELEASE_NOTES_TEST_DATA_DIR / f"github_release_notes_{TEST_GARDENLINUX_RELEASE}.md"
-    glvd_response_fixture_path = RELEASE_NOTES_TEST_DATA_DIR / f"glvd_{TEST_GARDENLINUX_RELEASE}.json"
+    release_fixture_path = (
+        RELEASE_NOTES_TEST_DATA_DIR
+        / f"github_release_notes_{TEST_GARDENLINUX_RELEASE}.md"
+    )
+    glvd_response_fixture_path = (
+        RELEASE_NOTES_TEST_DATA_DIR / f"glvd_{TEST_GARDENLINUX_RELEASE}.json"
+    )
 
     with requests_mock.Mocker(real_http=True) as m:
         for yaml_file in RELEASE_ARTIFACTS_METADATA_FILES:
             filepath = f"{RELEASE_NOTES_S3_ARTIFACTS_DIR}/{yaml_file}"
-            base = yaml_file[:-len(".s3_metadata.yaml")]
+            base = yaml_file[: -len(".s3_metadata.yaml")]
             key = f"meta/singles/{base}-{TEST_GARDENLINUX_RELEASE}-{TEST_GARDENLINUX_COMMIT}"
             release_s3_bucket.upload_file(filepath, key)
 
         m.get(
             f"{GLVD_BASE_URL}/patchReleaseNotes/{TEST_GARDENLINUX_RELEASE}",
             text=glvd_response_fixture_path.read_text(),
-            status_code=200
+            status_code=200,
         )
-        generated_release_notes = gardenlinux.github.release_notes.create_github_release_notes(
-            TEST_GARDENLINUX_RELEASE,
-            TEST_GARDENLINUX_COMMIT,
-            release_s3_bucket.name
+        generated_release_notes = (
+            gardenlinux.github.release_notes.create_github_release_notes(
+                TEST_GARDENLINUX_RELEASE,
+                TEST_GARDENLINUX_COMMIT,
+                release_s3_bucket.name,
+            )
         )
 
         assert generated_release_notes == release_fixture_path.read_text()
