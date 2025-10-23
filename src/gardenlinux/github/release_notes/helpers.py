f237e18cdc65fa96d8bce05484a58e86d3b9f199
Vivus Ignis <ignis@posteo.de>
2025-10-23 15:42:22 +0200

    replaced unneeded short commit

--- src/gardenlinux/github/release_notes/helpers.py
+++ src/gardenlinux/github/release_notes/helpers.py
@@ -64,7 +64,7 @@ def download_all_metadata_files(version, commitish, s3_bucket_name):
     commitish_short = commitish[:8]
 
     for flavor in flavors:
-        cname = CName(flavor[1], flavor[0], commitish_short)
+        cname = CName(flavor[1], flavor[0], commitish)
         LOGGER.debug(f"{flavor=} {version=} {commitish=}")
         # Filter by image variants - only download if the flavor matches one of the variants
         flavor_matches_variant = False
