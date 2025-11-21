260ac9ebc19fc29c5a4de8fbddd956fb5604a82f
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-11-21 13:31:46 +0100

    Update GLVD endpoint (#261)

--- src/gardenlinux/constants.py
+++ src/gardenlinux/constants.py
@@ -162,7 +162,7 @@ REQUESTS_TIMEOUTS = (5, 30)  # connect, read
 S3_DOWNLOADS_DIR = Path(os.path.dirname(__file__)) / ".." / "s3_downloads"
 
 GLVD_BASE_URL = (
-    "https://glvd.ingress.glvd.gardnlinux.shoot.canary.k8s-hana.ondemand.com/v1"
+    "https://security.gardenlinux.org/v1"
 )
 GL_DEB_REPO_BASE_URL = "https://packages.gardenlinux.io/gardenlinux"
 
